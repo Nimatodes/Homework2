c11a43a0aa5a8b3f6970c0e778f8bf029ecddce8
Wynne <wynnetran@gmail.com>
2016-02-12 18:07:27 -0800

    add prodcon.c

--- prodcon.c
+++ prodcon.c
@@ -4,6 +4,8 @@
 #include <semaphore.h>
 
 #define BUFFERSIZE 1024
+#define TRUE 1
+#define FALSE 0
 
 void *producer(void *b);
 void *consumer(void *b);
@@ -15,9 +17,37 @@ pthread_t *consumerThreads;
 
 int **buffer;
 
-sem_t *mutexes;
+sem_t *mutex;
 sem_t *empty;
 sem_t *full;
+sem_t itemCount;
+
+void *producer(void *b) {
+	int item;
+
+	while (TRUE) {
+		//item = produce_item();
+		//sem_wait(&empty);
+		//sem_wait(&mutex);
+		//produce(buffer, item);	// insert item in buffer
+		//sem_post(&mutex);
+		//sem_post(&full);
+		//printf("Produced:");
+
+	}
+}
+
+void *consumer(void *b) {
+	
+}
+
+void produce(int *buffer, int i) {
+	
+}
+
+void consume(int *buffer, int i) {
+	
+}
 
 int main(int argc, char *argv[]) {
 	
@@ -33,21 +63,30 @@ int main(int argc, char *argv[]) {
 	int i = 0;
 
 	buffer = malloc(bufferCount * sizeof(int) * BUFFERSIZE);
+	mutex = malloc(bufferCount * sizeof(sem_t));
+	empty = malloc(bufferCount * sizeof(sem_t));
+	full = malloc(bufferCount * sizeof(sem_t));
+
+	for (i = 0; i < bufferCount; i++) {
+		sem_init(&mutex[i], 0, 1);
+		sem_init(&empty[i], 0, BUFFERSIZE);
+		sem_init(&full[i], 0, 0);
+	}
 
 	producerThreads = malloc(producerCount * sizeof(pthread_t));
 	for (i = 0; i < producerCount; i++) {
-		pthread_create(&producerThreads[i], NULL, &producer, buffer);	
+		pthread_create(&producerThreads[i], NULL, (void*)&producer, buffer);	
 	}
 
 	consumerThreads = malloc(consumerCount * sizeof(pthread_t));
 	for (i = 0; i < consumerCount; i++) {
-		pthread_create(&consumerThreads[i], NULL, &consumer, buffer);
+		pthread_create(&consumerThreads[i], NULL, (void*)&consumer, buffer);
 	}
 
 	free(producerThreads);
 	free(consumerThreads);
 	free(buffer);
-	free(mutexes);
+	free(mutex);
 	free(empty);
 	free(full);
 
